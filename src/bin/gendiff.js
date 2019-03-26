a4396d217b094395f4c87038473e396d61f3f1f6
DeltaSPb <oipts3012@gmail.com>
2019-03-26 11:53:28 +0300

    v 1.4.0

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,7 +3,7 @@ import program from 'commander';
 import genDiff from '..';
 
 program
-  .version('1.3.0')
+  .version('1.4.0')
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
