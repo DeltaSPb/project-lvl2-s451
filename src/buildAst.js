52cf0856f1af2fbc9f410ed0a1d4780439033e2b
DeltaSPb <oipts3012@gmail.com>
2019-03-26 11:52:54 +0300

    new logic of project work

---
+++ src/buildAst.js
@@ -0,0 +1,38 @@
+import _ from 'lodash';
+
+const existence = [
+  {
+    check: (key, obj1, obj2) => _.isObject(obj1[key]) && _.isObject(obj2[key]),
+    difference: (key, obj1, obj2, fn) => ({ state: 'nasted', key, children: fn(obj1[key], obj2[key]) }),
+  },
+  {
+    check: (key, obj1) => !_.has(obj1, key),
+    difference: (key, obj1, obj2) => ({ state: 'added', key, value: obj2[key] }),
+  },
+  {
+    check: (key, obj1, obj2) => !_.has(obj2, key),
+    difference: (key, obj1) => ({ state: 'removed', key, value: obj1[key] }),
+  },
+  {
+    check: (key, obj1, obj2) => obj1[key] === obj2[key],
+    difference: (key, obj1) => ({ state: 'unchanged', key, value: obj1[key] }),
+  },
+  {
+    check: (key, obj1, obj2) => obj1[key] !== obj2[key],
+    difference: (key, obj1, obj2) => ({
+      state: 'changed', key, oldValue: obj1[key], newValue: obj2[key],
+    }),
+  },
+];
+
+const checkExistence = (key, obj1, obj2) => existence.find(({ check }) => check(key, obj1, obj2));
+
+const buildAst = (obj1, obj2) => {
+  const keys = _.union(_.keys(obj1), _.keys(obj2));
+  const ast = keys.reduce((acc, key) => {
+    const { difference } = checkExistence(key, obj1, obj2);
+    return [...acc, difference(key, obj1, obj2, buildAst)];
+  }, []);
+  return ast;
+};
+export default buildAst;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import fs from 'fs';
-import _ from 'lodash';
 import path from 'path';
 import parse from './parsers';
+import render from './render';
+import buildAst from './buildAst';
+
 
 const readAndParse = (pathToCurrent) => {
   const content = fs.readFileSync(pathToCurrent, 'utf-8');
@@ -9,36 +11,9 @@ const readAndParse = (pathToCurrent) => {
   return parse(extention, content);
 };
 
-const existence = [
-  {
-    check: (key, obj1) => !_.has(obj1, key),
-    difference: (key, obj1, obj2) => `+ ${key}: ${obj2[key]}`,
-  },
-  {
-    check: (key, obj1, obj2) => !_.has(obj2, key),
-    difference: (key, obj1) => `- ${key}: ${obj1[key]}`,
-  },
-  {
-    check: (key, obj1, obj2) => obj1[key] === obj2[key],
-    difference: (key, obj1) => `    ${key}: ${obj1[key]}`,
-  },
-  {
-    check: (key, obj1, obj2) => obj1[key] !== obj2[key],
-    difference: (key, obj1, obj2) => [[`- ${key}: ${obj1[key]}`], [`+ ${key}: ${obj2[key]}`]],
-  },
-];
-
-const diffToString = (obj1, obj2) => {
-  const keys = _.union(_.keys(obj1), _.keys(obj2));
-  const result = keys.map((key) => {
-    const { difference } = existence.find(({ check }) => check(key, obj1, obj2));
-    return difference(key, obj1, obj2);
-  });
-  return `{\n${_.flattenDeep(result).join('\n  ')}\n}`;
-};
-
 export default (pathToFile1, pathToFile2) => {
-  const obj1 = readAndParse(pathToFile1);
-  const obj2 = readAndParse(pathToFile2);
-  return diffToString(obj1, obj2);
+  const data1 = readAndParse(pathToFile1);
+  const data2 = readAndParse(pathToFile2);
+  const ast = buildAst(data1, data2);
+  return render(ast);
 };
