be0acd43899bb40d71273aabc99a615db5e8d740
DeltaSPb <oipts3012@gmail.com>
2019-03-26 11:51:07 +0300

    add test for nested files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,15 +3,18 @@ import path from 'path';
 import genDiff from '../src';
 
 const filesArr = [
-  ['before.json', 'after.json'],
-  ['before.yaml', 'after.yaml'],
-  ['before.ini', 'after.ini'],
+  ['before.json', 'after.json', 'result.txt'],
+  ['before.yaml', 'after.yaml', 'result.txt'],
+  ['before.ini', 'after.ini', 'result.txt'],
+  ['before_nested.json', 'after_nested.json', 'result_nested.txt'],
+  ['before_nested.yaml', 'after_nested.yaml', 'result_nested.txt'],
+  ['before_nested.ini', 'after_nested.ini', 'result_nested.txt'],
 ];
 
-test.each(filesArr)('genDiff(%s, %s)', (fileBefore, fileAfter) => {
+test.each(filesArr)('genDiff(%s, %s)', (fileBefore, fileAfter, result) => {
   const pathToBeforeFile = path.resolve(__dirname, `__fixtures__/${fileBefore}`);
   const pathToAfterFile = path.resolve(__dirname, `__fixtures__/${fileAfter}`);
-  const pathToResult = path.resolve(__dirname, '__fixtures__/result.txt');
+  const pathToResult = path.resolve(__dirname, `__fixtures__/${result}`);
 
   const expected = fs.readFileSync(pathToResult, 'utf8').trim();
   expect(genDiff(pathToBeforeFile, pathToAfterFile)).toEqual(expected);
