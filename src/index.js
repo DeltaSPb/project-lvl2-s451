699b69e60fb44d0b2e7938cbf72494f94502f8be
DeltaSPb <oipts3012@gmail.com>
2019-03-27 16:42:14 +0300

    new logic of rendering

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import parse from './parsers';
-import render from './render';
+import render from './renders/render';
 import buildAst from './buildAst';
 
 
@@ -11,9 +11,9 @@ const readAndParse = (pathToCurrent) => {
   return parse(extention, content);
 };
 
-export default (pathToFile1, pathToFile2) => {
+export default (pathToFile1, pathToFile2, format) => {
   const data1 = readAndParse(pathToFile1);
   const data2 = readAndParse(pathToFile2);
   const ast = buildAst(data1, data2);
-  return render(ast);
+  return render(ast, format);
 };
