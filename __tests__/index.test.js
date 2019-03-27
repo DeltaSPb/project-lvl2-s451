5fad53366ea0e33d6430edc5cfdb5c2084d182e9
DeltaSPb <oipts3012@gmail.com>
2019-03-27 16:32:36 +0300

    add "format" as fourth argument

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,19 +3,19 @@ import path from 'path';
 import genDiff from '../src';
 
 const filesArr = [
-  ['before.json', 'after.json', 'result.txt'],
-  ['before.yaml', 'after.yaml', 'result.txt'],
-  ['before.ini', 'after.ini', 'result.txt'],
-  ['before_nested.json', 'after_nested.json', 'result_nested.txt'],
-  ['before_nested.yaml', 'after_nested.yaml', 'result_nested.txt'],
-  ['before_nested.ini', 'after_nested.ini', 'result_nested.txt'],
+  ['before.json', 'after.json', 'tree', 'resultTree.txt'],
+  ['before.yaml', 'after.yaml', 'tree', 'resultTree.txt'],
+  ['before.ini', 'after.ini', 'tree', 'resultTree.txt'],
+  ['before.json', 'after.json', 'plain', 'resultPlain.txt'],
+  ['before.yaml', 'after.yaml', 'plain', 'resultPlain.txt'],
+  ['before.ini', 'after.ini', 'plain', 'resultPlain.txt'],
 ];
 
-test.each(filesArr)('genDiff(%s, %s)', (fileBefore, fileAfter, result) => {
+test.each(filesArr)('genDiff(%s, %s)', (fileBefore, fileAfter, format, result) => {
   const pathToBeforeFile = path.resolve(__dirname, `__fixtures__/${fileBefore}`);
   const pathToAfterFile = path.resolve(__dirname, `__fixtures__/${fileAfter}`);
   const pathToResult = path.resolve(__dirname, `__fixtures__/${result}`);
 
   const expected = fs.readFileSync(pathToResult, 'utf8').trim();
-  expect(genDiff(pathToBeforeFile, pathToAfterFile)).toEqual(expected);
+  expect(genDiff(pathToBeforeFile, pathToAfterFile, format)).toEqual(expected);
 });
