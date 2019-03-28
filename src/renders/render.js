e6fa0379bcaad48e680defe9cb7b001a9f4f0cc5
DeltaSPb <oipts3012@gmail.com>
2019-03-28 13:31:06 +0300

    rm

--- src/renders/render.js
+++
@@ -1,9 +0,0 @@
-import renderTree from './renderTree';
-import renderPlain from './renderPlain';
-
-const renderTypes = {
-  tree: renderTree,
-  plain: renderPlain,
-};
-
-export default (ast, format) => renderTypes[format](ast);
