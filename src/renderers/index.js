582d5f2c6db09b5043fe560d4dc34937cd020d54
DeltaSPb <oipts3012@gmail.com>
2019-03-28 13:47:15 +0300

    added JSON type of render

--- src/renderers/index.js
+++ src/renderers/index.js
@@ -1,9 +1,11 @@
 import renderTree from './renderTree';
 import renderPlain from './renderPlain';
+import renderJSON from './renderJSON';
 
 const renderTypes = {
   tree: renderTree,
   plain: renderPlain,
+  json: renderJSON,
 };
 
 export default (ast, format) => renderTypes[format](ast);
